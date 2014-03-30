ee82b7f9da50afc94c7493d56a1ee5d2414de65a
Vadim Macagon <vadim.macagon@gmail.com>
2014-03-30 17:44:12 +0700

    Add basic module that does absolutely nothing.

---
+++ Source/MercurialSourceControl/MercurialSourceControl.Build.cs
@@ -0,0 +1,46 @@
+namespace UnrealBuildTool.Rules
+{
+	public class MercurialSourceControl : ModuleRules
+	{
+		public MercurialSourceControl(TargetInfo Target)
+		{
+			PublicIncludePaths.AddRange(
+				new string[] {
+					// ... add public include paths required here ...
+				}
+				);
+
+			PrivateIncludePaths.AddRange(
+				new string[] {
+					// ... add other private include paths required here ...
+				}
+				);
+
+			PublicDependencyModuleNames.AddRange(
+				new string[]
+				{
+					// ... add other public dependencies that you statically link with here ...
+				}
+				);
+
+			PrivateDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"Core",
+					"Slate",
+					"EditorStyle",
+					"SourceControl",
+					"XmlParser",
+					// ... add private dependencies that you statically link with here ...
+				}
+				);
+
+			DynamicallyLoadedModuleNames.AddRange(
+				new string[]
+				{
+					// ... add any modules that your module loads dynamically here ...
+				}
+				);
+		}
+	}
+}
